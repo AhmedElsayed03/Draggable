c09dbbed7f946612fb163317c263f6b47a8e3741
Ahmed.Ebrahim <Ahmed.Ebrahim@ETS10-HPLP-39>
2024-12-18 02:55:52 +0200

    fix

--- src/app/directives/drag-resize.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DragResizeDirective } from './drag-resize.directive';
-
-describe('DragResizeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DragResizeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
