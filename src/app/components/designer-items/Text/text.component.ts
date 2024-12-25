24c9c7d5c21fd2dfda8f1ef6e05b709e5b2a8942
Abdelrahman Mohamed <abdo.mohamed204650@gmail.com>
2024-12-25 17:03:35 +0200

    ..

--- src/app/components/designer-items/Text/text.component.ts
+++ src/app/components/designer-items/Text/text.component.ts
@@ -17,12 +17,12 @@ export class TextComponent implements OnInit, OnDestroy {
   toolbar: Toolbar = [
     ['bold', 'italic'],
     ['underline', 'strike'],
-    // ['code', 'blockquote'],
-    // ['ordered_list', 'bullet_list'],
-    // [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-    // ['link', 'image'],
-    // ['text_color', 'background_color'],
-    // ['align_left', 'align_center', 'align_right', 'align_justify'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
   ];
 
 
